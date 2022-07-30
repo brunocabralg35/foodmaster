6b91c0608c89a8922a8fb4ab43c6c3e7d4066a40
Bruno Cabral <brunocabral035@gmail.com>
2022-07-30 14:44:51 -0300

    created lateral menu with icons, and started to style cart

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,13 +1,42 @@
 import { useContext, useState } from "react";
 import MyContext from "../MyContext";
 import data from "../Assets/data.json";
+import { FaPizzaSlice, FaCandyCane, FaHamburger } from "react-icons/fa";
+import { GiFrenchFries } from "react-icons/gi";
+import { BiDrink } from "react-icons/bi";
 
 function Order() {
   const { user } = useContext(MyContext);
 
   return (
     <div className="order">
-      <div className="menu"></div>
+      <div className="menu">
+        <input type="radio" id="pizza" name="opcao_food" value="pizza" />
+        <label for="pizza">
+          <FaPizzaSlice size={30} />
+          Pizza
+        </label>
+        <input type="radio" id="bebidas" name="opcao_food" value="bebidas" />
+        <label for="bebidas">
+          <BiDrink size={30} />
+          Bebidas
+        </label>
+        <input type="radio" id="doces" name="opcao_food" value="doces" />
+        <label for="doces">
+          <FaCandyCane size={30} />
+          Doces
+        </label>
+        <input type="radio" id="combos" name="opcao_food" value="combos" />
+        <label for="combos">
+          <GiFrenchFries size={30} />
+          Combos
+        </label>
+        <input type="radio" id="burguers" name="opcao_food" value="burguers" />
+        <label for="burguers">
+          <FaHamburger size={30} />
+          Burguers
+        </label>
+      </div>
 
       <div className="food">
         <div className="cards">
@@ -25,7 +54,10 @@ function Order() {
         </div>
       </div>
 
-      <div className="cart"></div>
+      <div className="cart">
+        <h1>Olá, {user}</h1>
+        <p>Seu pedido</p>
+      </div>
     </div>
   );
 }
